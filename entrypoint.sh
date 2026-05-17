edbcdf44574033e4e5d789ff84630643484be60b
David Alexander <dmalexa5@ncsu.edu>
2026-05-17 14:02:36 +0000

    Remove uneccessary entrypoint file

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Clone dependencies into the workspace
-vcs import /ros2_ws/src < /ros2_ws/src/dependency.repos --recursive --skip-existing
-
-source /ros2_ws/.venv/bin/activate
-
-exec "$@"
