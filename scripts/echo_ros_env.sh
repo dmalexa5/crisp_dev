2c9b5cc3bc5155b615b92ab1a7dc7e716c04c101
David Alexander <dmalexa5@ncsu.edu>
2026-05-18 19:53:09 -0400

    remove unneccessary scripts

--- scripts/echo_ros_env.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-source /opt/ros/humble/setup.bash
-source install/setup.bash 2>/dev/null || true
-
-echo "ROS_DISTRO=${ROS_DISTRO:-}"
-echo "ROS_DOMAIN_ID=${ROS_DOMAIN_ID:-}"
-echo "RMW_IMPLEMENTATION=${RMW_IMPLEMENTATION:-}"
-
-which ros2
